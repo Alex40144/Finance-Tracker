0b1351a7e6bc57fc29f3b2a168854846ff6f4cbc
AlexPegg <alexpeggsailing@gmail.com>
2021-07-21 14:36:26 +0100

    moved from other project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+        blue: '#0070f3',
+        light: '#666',
+        white: '#FFFFFF',
+        black: '#111',
+        dark: '#363636'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
